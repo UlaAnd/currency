d6bc8356a5590a2af6efd70d482706058873095f
Ula_Andrz <ulandrzejczak@gmail.com>
2024-01-17 09:51:20 +0100

    Save data into a CSV file [sc-22]

--- currency/settings.py
+++ currency/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_tables2",
+    "request",
     "currency_app",
 ]
 
@@ -46,6 +47,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
+    "request.middleware.RequestMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -56,7 +58,7 @@ ROOT_URLCONF = "currency.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -118,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- currency_app/views.py
+++ currency_app/views.py
@@ -1,5 +1,9 @@
+import django_tables2 as tables
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django_tables2 import RequestConfig
+from django_tables2.export.export import TableExport
+from django_tables2.export.views import ExportMixin
 
 from .forms import ColumnVisibilityForm
 from .models import ExchangeRate
@@ -9,7 +13,14 @@ from .tables import ExchangeRateTable
 def exchange_rate(request: HttpRequest) -> HttpResponse:
     queryset = ExchangeRate.objects.all()
     table = ExchangeRateTable(queryset)
-    return render(request, "currency.html", {"table": table})
+    RequestConfig(request).configure(table)
+
+    export_format = request.GET.get("_export", None)
+    if TableExport.is_valid_format(export_format):
+        exporter = TableExport(export_format, table)
+        return exporter.response(f"table.{export_format}")
+
+    return render(request, "table.html", {"table": table})
 
 
 def exchange_rate_table(request: HttpRequest) -> HttpResponse:
@@ -28,15 +39,48 @@ def exchange_rate_table(request: HttpRequest) -> HttpResponse:
                 for field_name, is_visible in form.cleaned_data.items()
                 if not is_visible
             ]
-            table = ExchangeRateTable(
-                ExchangeRate.objects.all(), exclude=invisible_columns
-            )
-            form = ColumnVisibilityForm(
-                initial={field: False for field in invisible_columns}
-            )
-            return render(
-                request, "exchange_rate_table.html", {"table": table, "form": form}
-            )
+            request.session["invisible_columns"] = invisible_columns
     table = ExchangeRateTable(ExchangeRate.objects.all(), exclude=invisible_columns)
     form = ColumnVisibilityForm(initial={field: False for field in invisible_columns})
-    return render(request, "exchange_rate_table.html", {"table": table, "form": form})
+    export_format = request.GET.get("_export", None)
+    if export_format:
+        selected_invisible = request.session["invisible_columns"]
+        response = export_table(export_format, selected_invisible)
+        return response
+    else:
+        return render(
+            request,
+            "exchange_rate_table.html",
+            {"table": table, "form": form, "test": "huh"},
+        )
+
+
+def export_table(export_format: str, selected_invisible: list) -> HttpResponse:
+    if export_format == "csv_all":
+        table = ExchangeRateTable(ExchangeRate.objects.all())
+        exporter = TableExport("csv", table)
+        filename = "all_currency_data.csv"
+        confirmation_message = "Data for all currencies has been saved!"
+
+    elif export_format == "csv":
+        table = ExchangeRateTable(
+            ExchangeRate.objects.all(), exclude=selected_invisible
+        )
+        exporter = TableExport(export_format, table)
+        filename = "selected_currency_data.csv"
+        confirmation_message = "Data for selected currencies has been saved!"
+
+    else:
+        return None  # Invalid export format
+    response = exporter.response(f"table.{export_format}")
+    response["Content-Disposition"] = f'attachment; filename="{filename}"'
+    response["Content-Type"] = f"text/{export_format}"
+    response["confirmation_message"] = confirmation_message
+    return response
+
+
+class TableView(ExportMixin, tables.SingleTableView):
+    table_class = ExchangeRateTable
+    model = ExchangeRate
+    template_name = "exchange_rate_table.html"
+    export_formats = ["csv"]
